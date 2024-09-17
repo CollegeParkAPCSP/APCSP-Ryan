9d7cab4d8e638f4a0df4b7e6da5cbda5955c74b4
StarlessKnights <StarlessKnights@users.noreply.github.com>
2024-09-17 12:46:25 -0500

    3.1

---
+++ Unit-3/3.1/main.py
@@ -0,0 +1,18 @@
+# Below is a single print statement
+# Add escape sequences such that the single print statement
+# outputs the same as the multiline print statement
+# Single line print
+print('\\Always avoid writing while (true) ...\n lest your code gets you in a stew.\n\\It can also become quite \'tedious\'\nMakes users want to fall under a bus\n\n\\Avoid the pizza wheel of death\n It gets your users in a mess\n\\And whatever else you do\nAlways avoid writing while (true)')
+# Multiline print
+print("")
+print("Make the poem match the format below")
+print("")
+print("\\Always avoid writing while (true) ...")
+print(" lest your code gets you in a stew.")
+print("\\It can also become quite 'tedious'")
+print(" Makes users want to fall under a bus")
+print("")
+print("\\Avoid the pizza wheel of death ")
+print(" It gets your users in a mess")
+print("\\And whatever else you do")
+print(" Always avoid writing while (true)")
