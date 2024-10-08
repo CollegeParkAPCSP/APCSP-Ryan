75b7f4412c1c70d98b531c893dbcfa646b439275
StarlessKnights <StarlessKnights@users.noreply.github.com>
2024-10-08 10:27:48 -0500

    Day 2 Proj

--- 1stNineWeeksProj/main.py
+++ 1stNineWeeksProj/main.py
@@ -70,15 +70,66 @@ def draw_reg_decagon(x, y, turtle, size):
     turtle.penup()
     
 # ----- END DAY 1 -----
-draw_square(-60, 60, tima, 30)
+# draw_square(-60, 60, tima, 30)
 
-draw_circle(0, 60, tima, 15)
+# draw_circle(0, 60, tima, 15)
 
-draw_triangle(60, 60, tima, 10)
+# draw_triangle(60, 60, tima, 10)
 
-draw_reg_pentagon(-60, -60, tima, 20)
+# draw_reg_pentagon(-60, -60, tima, 20)
 
-draw_reg_hexagon(0, -60, tima, 15)
+# draw_reg_hexagon(0, -60, tima, 15)
 
-draw_reg_decagon(60, -60, tima, 8)
+# draw_reg_decagon(60, -60, tima, 8)
 
+# ----- START DAY 2 -----
+def draw_shape(shape, x, y, size):
+    if shape == "square":
+        draw_square(x, y, tima, size)
+    elif shape == "circle":
+        draw_circle(x, y, tima, size)
+    elif shape == "pentagon":
+        draw_reg_pentagon(x, y, tima, size)
+    elif shape == "triangle":
+        draw_triangle(x, y, tima, size)
+    elif shape == "hexagon":
+        draw_reg_hexagon(x, y, tima, size)
+    elif shape == "decagon":
+        draw_reg_decagon(x, y, tima, size)
+    else:
+        print("Invalid shape")
+        return 0
+        
+    return calc_area(shape, size)
+        
+def calc_area(shape, size):
+    if shape == "square":
+        return size ** 2
+    elif shape == "circle":
+        return math.pi * size ** 2
+    elif shape == "pentagon":
+        return 1/4 * math.sqrt(5 * (5 + 2 * math.sqrt(5))) * size ** 2
+    elif shape == "triangle":
+        return 1/2 * size ** 2 * math.sqrt(3) / 2
+    elif shape == "hexagon":
+        return 3/2 * math.sqrt(3) * size ** 2
+    elif shape == "decagon":
+        return 5/2 * size ** 2 * math.sqrt(5 + 2 * math.sqrt(5))
+    else:
+        print("Invalid shape")
+        return 0
+    
+draw_shape("square", -60, 60, 30)
+
+draw_shape("circle", 0, 60, 15)
+
+draw_shape("triangle", 60, 60, 10)
+
+draw_shape("pentagon", -60, -60, 20)
+
+draw_shape("hexagon", 0, -60, 15)
+
+draw_shape("decagon", 60, -60, 8)
+
+turtle.done()
+# ----- END DAY 2 -----
