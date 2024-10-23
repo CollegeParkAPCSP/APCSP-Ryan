8910bf808508ceefbecb3d2fed2ca33c013c5d02
StarlessKnights <StarlessKnights@users.noreply.github.com>
2024-10-23 10:23:15 -0500

    5.4 fix

--- Unit-5/5.4/main.py
+++ Unit-5/5.4/main.py
@@ -1,16 +1,16 @@
 # using isalnum isalpha isdigit islower isupper
 
-def find_sub(str, substr):
-    if str.find(substr) == -1:
+def find_sub(string, substr):
+    if string.find(substr) == -1:
         print("Substring is not in string")
         return -1
     
-    print(f"\nThe substring was found at character {str.find(substr) + 1}")
+    print(f"\nThe substring was found at character {string.find(substr) + 1}")
     
-    print(f"all alphanumeric? {substr.isalnum()}")
-    print(f"all alphabetic? {substr.isalpha()}")
-    print(f"all digits? {substr.isdigit()}")
-    print(f"all lowercase? {substr.islower()}")
-    print(f"all uppercase? {substr.isupper()}\n")
+    print(f"all alphanumeric? {string.isalnum()}")
+    print(f"all alphabetic? {string.isalpha()}")
+    print(f"all digits? {string.isdigit()}")
+    print(f"all lowercase? {string.islower()}")
+    print(f"all uppercase? {string.isupper()}\n")
 
 find_sub("hello", "lo")
